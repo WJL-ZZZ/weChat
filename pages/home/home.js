a1fd1e04f9819f4902dd3d138949fc960bd88aa2
wjl <18013014021@163.com>
2018-05-26 00:31:31 +0800

    save data 1 has a bug

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,20 +1,40 @@
-//获取应用实例
-var money = "0"
+//获取应用实例ß
 const util = require('../../utils/util.js')
+
+var homeMoney = "0"
+var time ="0"
+var dailyMoney = {
+  time: "",
+  dailyMoney: "0",
+  readySave: false,
+}
 const conf = {
   data: {
-    money: money
+    money: "0"
   },
   // 初始化数据
   onLoad(options) {
-
+    var today = new Date();
+    time = util.formatTime(today)
+    var mDailyMoney = util.getListByTime(time) || {};
+    if (mDailyMoney != {}) {
+      dailyMoney = mDailyMoney
+    }
+    homeMoney = dailyMoney.money || "0"
+    this.setData({
+      money: homeMoney
+    })
   },
+
   getRadom: function (e) {
-    if (money == "0") {
-      money = util.getRadom(365)
+    if (homeMoney == "0") {
+      homeMoney = util.getRadom(365)
       this.setData({
-        money: money
+        money: homeMoney
       })
+      dailyMoney.time = time
+      dailyMoney.money = homeMoney
+      util.setList(dailyMoney)
     }
   }
 };
--- utils/util.js
+++ utils/util.js
@@ -6,19 +6,66 @@ const formatTime = date => {
   const minute = date.getMinutes()
   const second = date.getSeconds()
 
-  return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
+  return [year, month, day].map(formatNumber).join('/')
 }
 
 const formatNumber = n => {
   n = n.toString()
   return n[1] ? n : '0' + n
 }
-const getRadom = n =>{
+const getRadom = n => {
   return Math.floor(Math.random() * n + 1)
 
 }
 
+const setList = dataItem => {
+  var dailyMoneyList = getList() || new Array();
+  for (let i in dailyMoneyList) {
+    if (dailyMoneyList[i] == dataItem) {  //如果数组中有该城市，flage变为false  
+      return false;
+    }
+  }
+
+  dailyMoneyList.push(dataItem);
+  wx.setStorage({
+    key: "dailyMoney",
+    data: dailyMoneyList,
+  })
+}
+
+const getList = key => {
+  wx.getStorage({
+    key: "dailyMoney",
+    success: function (res) {
+      return res.data || []
+    },
+  })
+}
+const getListByTime = time => {
+  wx.getStorage({
+    key: "dailyMoney",
+    success: function (res) {
+      var dailyMoneyList = res.data
+      if (dailyMoneyList != []) {
+        for (let i in dailyMoneyList) {
+          if (dailyMoneyList[i].time == time) {  //如果数组中有该城市，flage变为false  
+            return dailyMoneyList[i];
+          }
+        }
+      }
+      return {
+        time: "",
+        dailyMoney: "0",
+        readySave: false,
+      }
+    },
+  })
+}
+
 module.exports = {
   formatTime: formatTime,
-  getRadom: getRadom
+  getRadom: getRadom,
+  getList: getList,
+  setList: setList,
+  getListByTime: getListByTime,
 }
